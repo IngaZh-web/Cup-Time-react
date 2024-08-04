bc66a84fc6526e31e48760c6339730e6ae52f01e
Inga Zhelnova <detal.07@mail.ru>
2024-08-04 17:23:42 +0300

    work with the project

--- src/modules/Cart.jsx
+++ src/modules/Cart.jsx
@@ -1,22 +1,34 @@
-import { products } from "../products";
-import { CartItem } from "./CartItem"
+import { useCart } from "../context/CartContext";
+import { CartItem } from "./CartItem";
+import { SkeletonLoader } from './SkeletonLoader'
 
-export const Cart = () => (
-  <section className="cart">
-    <div className="container cart__container">
-      <h2 className="cart__title">Корзина (6)</h2>
+export const Cart = () => {
+  const { cart } = useCart();
+	console.log('cart: ', cart);
 
-      <ul className="cart__items">
-        {products.map((item) => (
-          <CartItem key={item.id} data={item} />
-        ))}
-      </ul>
+  const totalPrise = cart ? cart.reduce((acc, item) => item.quantity * item.price + acc, 0) : 0;
 
-			<div className="cart__summary">
-				<h3 className="cart__summary-title">Итого:</h3>
-				<p className="cart__total">2200&nbsp;₽</p>
-				<button className="cart__order-button">Заказать</button>
-			</div>
-    </div>
-  </section>
-);
+  return (
+    <section className="cart">
+      <div className="container cart__container">
+        <h2 className="cart__title">Корзина ({cart.length})</h2>
+
+        <ul className="cart__items">
+          {cart ? cart.map((item) => (
+            <CartItem key={item.id} data={item} />
+          )) : (
+						<SkeletonLoader />
+					)}
+
+
+        </ul>
+
+        <div className="cart__summary">
+          <h3 className="cart__summary-title">Итого:</h3>
+          <p className="cart__total">{totalPrise}&nbsp;₽</p>
+          <button className="cart__order-button">Заказать</button>
+        </div>
+      </div>
+    </section>
+  );
+};
